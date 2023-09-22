b99d1be6d34cdbd373448eb9e41dd937691bd4b1
Jonathan Ato Markin <jona10711@gmail.com>
2023-09-22 09:59:48 +0200

    Mantine ui removed from project

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,5 @@
 import "@/styles/globals.css";
-import "@mantine/core/styles.css";
-import VecAppShell from "@/components/VecAppShell";
-
-import { MantineProvider, createTheme } from "@mantine/core";
-
-const theme = createTheme({
-  /** Put your mantine theme override here */
-  fontFamily: "Lato, sans-serif",
-});
 
 export default function App({ Component, pageProps }) {
-  return (
-    <MantineProvider theme={theme}>
-      <VecAppShell>
-        <Component {...pageProps} />
-      </VecAppShell>
-    </MantineProvider>
-  );
+  return <Component {...pageProps} />;
 }
